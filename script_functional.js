73d344d85d7d71c49c9f0aeda60394fb7c2987a9
LucasAndria <lucasandria01@gmail.com>
2024-10-10 15:02:31 +0300

    change into real interactive dot

--- script_functional.js
+++ script_functional.js
@@ -2,15 +2,14 @@
 function cursor(options = []) {
   const cursor = {
     delay: 8,
-    _x: 0,
-    _y: 0,
+    outlineX: 0,
+    outlineY: 0,
     endX: window.innerWidth / 2,
     endY: window.innerHeight / 2,
     endScrollX: window.scrollX || 0,
     endScrollY: window.scrollY || 0,
     cursorVisible: true,
     cursorEnlarged: false,
-    showClientCursor: false,
     dot: document.querySelector(".cursor-dot"),
     outline: document.querySelector(".cursor-dot-outline"),
     rootStyles: document.querySelector(":root").style,
@@ -19,18 +18,73 @@ function cursor(options = []) {
       .style.getPropertyValue("--primary"),
   };
 
-  setupEventListeners(cursor, options);
+  setupBaseEventListener(cursor, options);
+  setupCursorStyle(options);
   animateDotOutline(cursor);
+
   return cursor;
+}
+
+function setupBaseEventListener(cursor, options) {
+  // Show custom cursor when client cursor enter the page
+  document.addEventListener("mouseenter", () => {
+    cursor.cursorVisible = true;
+    triggerVisibility(cursor);
+  });
+
+  // Hide custom cursor when client cursor quit the page
+  document.addEventListener("mouseleave", () => {
+    cursor.cursorVisible = false;
+    triggerVisibility(cursor);
+  });
+
+  // Track the client cursor's position
+  document.addEventListener("mousemove", (e) => {
+    cursor.cursorVisible = true;
+
+    // Position the dot
+    cursor.endX = e.clientX;
+    cursor.endY = e.clientY;
+
+    cursor.dot.style.top = `${cursor.endY + cursor.endScrollY}px`;
+    cursor.dot.style.left = `${cursor.endX + cursor.endScrollX}px`;
+
+    elementHoverEffect(cursor, options);
+  });
+
+  // Track the scroll and add to the calcul
+  document.addEventListener("scroll", () => {
+    cursor.endScrollY = window.scrollY; // Vertical scroll
+    cursor.endScrollX = window.scrollX; // Horizontal scroll
+
+    cursor.dot.style.top = `${cursor.endScrollY + cursor.endY}px`;
+    cursor.dot.style.left = `${cursor.endScrollX + cursor.endX}px`;
+
+    elementHoverEffect(cursor, options);
+  });
+
+  // Click on DOM events
+  document.addEventListener("mousedown", () => {
+    cursor.cursorEnlarged = true;
+    triggerEnlarged(cursor);
+  });
+
+  document.addEventListener("mouseup", () => {
+    cursor.cursorEnlarged = false;
+    triggerEnlarged(cursor);
+    elementHoverEffect(cursor, options);
+  });
 }
 
 // Function that will make the outline follow the dot
 function animateDotOutline(cursor) {
-  cursor._x += (cursor.endX + cursor.endScrollX - cursor._x) / cursor.delay;
-  cursor._y += (cursor.endY + cursor.endScrollY - cursor._y) / cursor.delay;
+  cursor.outlineX +=
+    (cursor.endX + cursor.endScrollX - cursor.outlineX) / cursor.delay;
+  cursor.outlineY +=
+    (cursor.endY + cursor.endScrollY - cursor.outlineY) / cursor.delay;
 
-  cursor.outline.style.top = `${cursor._y}px`;
-  cursor.outline.style.left = `${cursor._x}px`;
+  cursor.outline.style.top = `${cursor.outlineY}px`;
+  cursor.outline.style.left = `${cursor.outlineX}px`;
 
   requestAnimationFrame(() => animateDotOutline(cursor));
 }
@@ -45,12 +99,6 @@ function triggerEnlarged(cursor) {
   })`;
 }
 
-// Function that will show / hide the cursor with opacity (used only for mouse move)
-function triggerOpacity(cursor) {
-  cursor.dot.style.opacity = cursor.cursorVisible ? 1 : 0;
-  cursor.outline.style.opacity = cursor.cursorVisible ? 1 : 0;
-}
-
 // Function that will show / hide the cursor
 function triggerVisibility(cursor) {
   cursor.dot.style.transform = `translate(-50%, -50%) scale(${
@@ -61,108 +109,85 @@ function triggerVisibility(cursor) {
   })`;
 }
 
-function setupEventListeners(cursor, options) {
-  // Click on DOM events
-  document.addEventListener("mousedown", () => {
-    cursor.cursorEnlarged = true;
-    triggerEnlarged(cursor);
+// Setup all the eventListener
+function elementHoverEffect(cursor, options) {
+  // Get the element under the cursor
+  const hoveredElement = document.elementFromPoint(cursor.endX, cursor.endY);
+  let matchElement = false;
+
+  // If there are no element hovered then break the function
+  if (!hoveredElement) return;
+
+  // Setup all eventListener that will trigger all users options
+  options?.forEach((option) => {
+    if (hoveredElement.matches(option.selector)) {
+      matchElement = true;
+
+      // Add enlarged effect
+      if (option.enlarged) {
+        cursor.cursorEnlarged = true;
+        triggerEnlarged(cursor);
+      }
+
+      // Add the client cursor effect
+      if (option.cursor) {
+        cursor.cursorVisible = false;
+        triggerVisibility(cursor);
+      }
+
+      // Add outline color effect
+      if (option.color) {
+        cursor.rootStyles.setProperty("--primary", option.color);
+      }
+    }
   });
 
-  document.addEventListener("mouseup", () => {
-    cursor.cursorEnlarged = false;
-    triggerEnlarged(cursor);
-  });
+  if (!matchElement) resetCursor(cursor);
+}
 
-  // Show custom cursor and track the client cursor position
-  document.addEventListener("mousemove", (e) => {
-    // Show the cursor
-    cursor.cursorVisible = true;
-    triggerOpacity(cursor);
+// Reset the cursor to its default state
+function resetCursor(cursor) {
+  cursor.delay = 8;
+  cursor.cursorVisible = true;
+  cursor.cursorEnlarged = false;
+  cursor.rootStyles.setProperty("--primary", cursor.defaultPrimary);
+  triggerVisibility(cursor);
+  triggerEnlarged(cursor);
+}
 
-    // Position the dot
-    cursor.endX = e.clientX;
-    cursor.endY = e.clientY;
-
-    cursor.dot.style.top = `${cursor.endY + cursor.endScrollY}px`;
-    cursor.dot.style.left = `${cursor.endX + cursor.endScrollX}px`;
-  });
-
-  // Track the scroll and add to the calcul
-  document.addEventListener("scroll", () => {
-    cursor.endScrollY = window.scrollY; // Vertical scroll
-    cursor.endScrollX = window.scrollX; // Horizontal scroll
-
-    cursor.dot.style.top = `${cursor.endScrollY + cursor.endY}px`;
-    cursor.dot.style.left = `${cursor.endScrollX + cursor.endX}px`;
-  });
-
-  // Show custom cursor when client cursor enter the page
-  document.addEventListener("mouseenter", () => {
-    cursor.cursorVisible = true;
-    triggerVisibility(cursor);
-    triggerOpacity(cursor);
-  });
-
-  // Hide custom cursor when client cursor quit the page
-  document.addEventListener("mouseleave", () => {
-    cursor.cursorVisible = false;
-    triggerVisibility(cursor);
-    triggerOpacity(cursor);
-  });
-
-  // Setup all custom event listener
+// Add the element cursor's style
+function setupCursorStyle(options) {
+  let cursorStyles;
+  // For each options
   options.forEach((option) => {
-    if (!option.selector) return;
-    // Create variables that will be used for element interactions
-    let effect_enlarged = option.enlarged || true;
-    let effect_cursor = option.cursor;
-    let effect_color = option.color;
+    // Get rid of possible html injection
+    const sanitizedSelector = sanitizeText(option.selector);
+    const sanitizedCursor = sanitizeText(option.cursor);
+    if (!sanitizedSelector || !sanitizedCursor) return;
 
-    document.querySelectorAll(option.selector).forEach((el) => {
-      // Ajout du condition pour l'éffet élarigir
-      if (effect_enlarged) {
-        el.addEventListener("mouseover", () => {
-          cursor.cursorEnlarged = true;
-          triggerEnlarged(cursor);
-          el.style.cursor = "none";
-        });
-        el.addEventListener("mouseout", () => {
-          cursor.cursorEnlarged = false;
-          triggerEnlarged(cursor);
-        });
-      }
+    // change the cursor for the selected element
+    document
+      .querySelectorAll(sanitizedSelector)
+      .forEach((el) => (el.style.cursor = sanitizedCursor));
 
-      // Cacher le custom cursor et afficher le client cursor
-      if (effect_cursor) {
-        el.addEventListener("mouseover", () => {
-          cursor.cursorVisible = false;
-          triggerVisibility(cursor);
-          el.style.cursor = effect_cursor || "auto";
-        });
-        el.addEventListener("mouseout", () => {
-          cursor.cursorVisible = true;
-          triggerVisibility(cursor);
-          el.style.cursor = "none";
-        });
-
-        // Show on click, then hide after
-        el.addEventListener("mouseup", (e) => {
-          e.stopPropagation();
-          cursor.cursorVisible = false;
-          triggerVisibility(cursor);
-          el.style.cursor = custom.cursor || "auto";
-        });
-      }
-
-      // Ajout de l'éffet qui change la couleur du custom cursor
-      if (effect_color) {
-        el.addEventListener("mouseover", () => {
-          cursor.rootStyles.setProperty("--primary", effect_color);
-        });
-        el.addEventListener("mouseout", () => {
-          cursor.rootStyles.setProperty("--primary", cursor.defaultPrimary);
-        });
-      }
-    });
+    // Create the style for all child element of the selected element
+    cursorStyles = cursorStyles
+      ? `${cursorStyles}
+    ${sanitizedSelector} * { cursor : ${sanitizedCursor}}`
+      : `${sanitizedSelector} * { cursor : ${sanitizedCursor}}`;
   });
+
+  // If there are no style to apply then break the function
+  if (!cursorStyles) return;
+
+  // Create a style that will add the pointer style and add it to the header
+  const style = document.createElement("style");
+  const cssRule = document.createTextNode(cursorStyles);
+  style.appendChild(cssRule);
+  document.head.appendChild(style);
+}
+
+// Allow valid CSS selector characters (alphanumeric, hyphens, ...)
+function sanitizeText(selector) {
+  return selector?.replace(/[^a-zA-Z0-9-_ .#*,=\[\]:()]/g, "");
 }
